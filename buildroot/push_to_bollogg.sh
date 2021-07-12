efeeac57da38bf00d1db97e414d139877534273d
Christopher Getschmann <christopher@volzo.de>
2021-07-12 20:23:51 +0200

    buildroot: rename tlpic to tlp

--- buildroot/push_to_bollogg.sh
+++ buildroot/push_to_bollogg.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DIR="bollogg:/home/volzotan/buildroot_extree_tlpic/external_tree/"
+DIR="bollogg:/home/volzotan/buildroot_extree_tlp/external_tree/"
 
-rsync -av config_tlpic/ $DIR --delete --exclude=".DS_Store"
+rsync -av config_tlp/ $DIR --delete --exclude=".DS_Store"
 
-rsync -av ../tlc $DIR"overlay/home/pi"              \
+rsync -av ../tlp $DIR"overlay/home/pi"              \
     --delete                                        \
     --exclude=".DS_Store"                           \
     --exclude="README.md"                           \
