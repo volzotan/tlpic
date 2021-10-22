6132aa45e65c5577c83107998028ce41201e2c9b
Christopher Getschmann <christopher@volzo.de>
2021-10-22 14:28:29 +0200

    generate: argparse for perlin

--- generate/perlin.py
+++ generate/perlin.py
@@ -2,14 +2,14 @@ from PIL import Image, ImageOps, ImageFilter
 import numpy as np
 import matplotlib.pyplot as plt
 import pyvista as pv
+import argparse
 
-DIMENSIONS      = [60, 110]
-PERLIN_PER_UNIT = 0.09
 PERLIN_PER_UNIT = 0.2
 SAMPLING_RATE   = 10 # per unit
 Z_HEIGHT        = 10
 
 BLOCK_HEIGHT    = Z_HEIGHT*2
+OUTPUT_FILENAME = "out.png"
 
 def perlin(x, y, seed=0):
 
@@ -74,46 +74,61 @@ def gradient(h,x,y):
     g = vectors[h%4]
     return g[:, :, 0] * x + g[:, :, 1] * y
 
-linx = np.linspace(0, DIMENSIONS[0]*PERLIN_PER_UNIT, DIMENSIONS[0]*SAMPLING_RATE, endpoint=False)
-liny = np.linspace(0, DIMENSIONS[1]*PERLIN_PER_UNIT, DIMENSIONS[1]*SAMPLING_RATE, endpoint=False)
+
+ap = argparse.ArgumentParser(description="Generate Perlin Noise")
+ap.add_argument("-x",    type=int, default=100, help="width")
+ap.add_argument("-y",    type=int, default=100, help="depth")
+ap.add_argument("-z",    type=float, default=Z_HEIGHT, help="height")
+ap.add_argument("-ppu",  type=float, default=PERLIN_PER_UNIT, help="perlin per unit")
+ap.add_argument("-s", "--sampling-rate", type=int, default=SAMPLING_RATE, help="number of points per unit (width/height) [int]")
+ap.add_argument("-o", "--output-filename", default=OUTPUT_FILENAME, help="output filename")
+ap.add_argument("--cutoff", action="store_true", default=False, help="cut off positive values")
+    
+args = vars(ap.parse_args())
+
+DIMENSIONS = [args["x"], args["y"]]
+
+linx = np.linspace(0, DIMENSIONS[0]*args["ppu"], DIMENSIONS[0]*args["sampling_rate"], endpoint=False)
+liny = np.linspace(0, DIMENSIONS[1]*args["ppu"], DIMENSIONS[1]*args["sampling_rate"], endpoint=False)
 x, y = np.meshgrid(linx, liny)
 
 res = perlin(x, y, seed=2)
 
 # cut off the hills, keep the valleys
-res[res[:,:] > 0] = 0 
+if args["cutoff"]:
+    res[res[:,:] > 0] = 0 
 
-plt.imsave("test.png", res, origin='upper')
+plt.imsave(args["output_filename"], res, origin="upper")
 
-# exit()
+exit()
 
 with open("p.xyz", "w") as f:
     grid = pv.StructuredGrid(x, y, res)
     for p in grid.points:
-        f.write("{} {} {}\n".format(p[0]/PERLIN_PER_UNIT, p[1]/PERLIN_PER_UNIT, p[2]*Z_HEIGHT))
+        f.write("{} {} {}\n".format(p[0]/args["ppu"], p[1]/args["ppu"], p[2]*Z_HEIGHT))
 
     ## additional points
     # side L/R
 
-    for i in range(0, DIMENSIONS[1]*SAMPLING_RATE):
-        for j in [0, DIMENSIONS[0]*SAMPLING_RATE-1]:
-            zs = np.linspace(-BLOCK_HEIGHT, res[i, j]*Z_HEIGHT, BLOCK_HEIGHT*SAMPLING_RATE, endpoint=False)
+    for i in range(0, DIMENSIONS[1]*args["sampling_rate"]):
+        for j in [0, DIMENSIONS[0]*args["sampling_rate"]-1]:
+            zs = np.linspace(-BLOCK_HEIGHT, res[i, j]*Z_HEIGHT, BLOCK_HEIGHT*args["sampling_rate"], endpoint=False)
             for z in zs:
-                f.write("{} {} {}\n".format(j/SAMPLING_RATE, i/SAMPLING_RATE, z))
+                f.write("{} {} {}\n".format(j/args["sampling_rate"], i/args["sampling_rate"], z))
 
     # side T/B
 
-    for i in [0, DIMENSIONS[1]*SAMPLING_RATE-1]:
-        for j in range(0, DIMENSIONS[0]*SAMPLING_RATE):
-            zs = np.linspace(-BLOCK_HEIGHT, res[i, j]*Z_HEIGHT, BLOCK_HEIGHT*SAMPLING_RATE, endpoint=False)
+    for i in [0, DIMENSIONS[1]*args["sampling_rate"]-1]:
+        for j in range(0, DIMENSIONS[0]*args["sampling_rate"]):
+            zs = np.linspace(-BLOCK_HEIGHT, res[i, j]*Z_HEIGHT, BLOCK_HEIGHT*args["sampling_rate"], endpoint=False)
             for z in zs:
-                f.write("{} {} {}\n".format(j/SAMPLING_RATE, i/SAMPLING_RATE, z))
+                f.write("{} {} {}\n".format(j/args["sampling_rate"], i/args["sampling_rate"], z))
 
     # bottom plane
 
     for j in linx[1:]:
         for i in liny[1:]:
-            f.write("{} {} {}\n".format(j/PERLIN_PER_UNIT, i/PERLIN_PER_UNIT, -BLOCK_HEIGHT))
+            f.write("{} {} {}\n".format(j/args["ppu"], i/args["ppu"], -BLOCK_HEIGHT))
 
 exit()
 
@@ -127,6 +142,7 @@ with Image.open("overlay.png") as im:
 
     result = Image.fromarray((base * 255).astype(np.uint8))
     result.save("out.png")
+
 
 # grid = pv.StructuredGrid(x, y, res)
 # # grid.plot(show_edges=True)
